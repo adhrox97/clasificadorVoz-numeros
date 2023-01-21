e26924297575085e95950bb3726304a65ff2aa9f
adhrox97 <57478421+adhrox97@users.noreply.github.com>
2023-01-20 23:12:13 -0500

    Add files via upload

---
+++ grabarAudio.py
@@ -0,0 +1,41 @@
+#EJEMPLO DE GRABADORA DE SONIDO CON "pyaudio".
+#IMPORTAMOS LIBRERIAS NECESARIAS
+import pyaudio
+import wave
+
+#DEFINIMOS PARAMETROS
+FORMAT=pyaudio.paInt16
+CHANNELS=1
+RATE=16000
+CHUNK=1024
+duracion=2
+archivo="dataset_num/dos/dos20.wav"
+
+#INICIAMOS "pyaudio"
+audio=pyaudio.PyAudio()
+
+#INICIAMOS GRABACIÓN
+stream=audio.open(format=FORMAT,channels=CHANNELS,
+                    rate=RATE, input=True,
+                    frames_per_buffer=CHUNK)
+
+print("grabando...")
+frames=[]
+
+for i in range(0, int(RATE/CHUNK*duracion)):
+    data=stream.read(CHUNK)
+    frames.append(data)
+print("grabación terminada")
+
+#DETENEMOS GRABACIÓN
+stream.stop_stream()
+stream.close()
+audio.terminate()
+
+#CREAMOS/GUARDAMOS EL ARCHIVO DE AUDIO
+waveFile = wave.open(archivo, 'wb')
+waveFile.setnchannels(CHANNELS)
+waveFile.setsampwidth(audio.get_sample_size(FORMAT))
+waveFile.setframerate(RATE)
+waveFile.writeframes(b''.join(frames))
+waveFile.close()
